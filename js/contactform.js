ed34de54d17d2dcedeb7bc83addc6533dbf8d0d1
amalfrost <44580362+amalfrost@users.noreply.github.com>
2020-09-25 23:15:05 +0530

    Add files via upload

---
+++ js/contactform.js
@@ -0,0 +1,19 @@
+<!-- <script>
+					$("#submit-form").submit((e)=>{
+						e.preventDefault()
+						$.ajax({
+							url:"https://script.google.com/macros/s/AKfycbyaXSPsfSK77aHjxXtnbzuVEDbaT_X9U6xIVPuQ/exec",
+							data:$("#submit-form").serialize(),
+							method:"post",
+							success:function (response){
+								alert("Form submitted successfully")
+								window.location.reload()
+								window.location.href="index.html"
+							},
+							error:function (err){
+								alert("Something Error")
+				
+							}
+						})
+					})
+				</script> -->
